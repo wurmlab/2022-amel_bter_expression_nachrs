601136dc1970e01fed58701afa85f22c7fe25fe9
Federico López-Osorio <flopezo84@gmail.com>
2022-09-13 17:23:06 +0100

    Update packages for plotting

--- renv/install_packages_create_renv_snapshot.R
+++ renv/install_packages_create_renv_snapshot.R
@@ -1,3 +1,5 @@
+install.packages("renv")
+
 renv::init(
   project = "~/projects/2020_amel_bter_expression_nachrs",
   bare = TRUE, restart = TRUE
@@ -12,9 +14,10 @@ load_cran_pkgs <- function(pkg) {
 }
 
 cran_pkgs <- c(
-  "devtools", "XML", "BiocManager", "tidyverse", "GGally", "ggtext",
-  "patchwork", "RColorBrewer",  "pheatmap", "styler", "renv", "here"
+  "devtools", "XML", "BiocManager", "tidyverse", "GGally", "ggtext", "ggh4x",
+  "ggforce", "egg", "patchwork", "RColorBrewer",  "styler", "here"
 )
+
 load_cran_pkgs(cran_pkgs)
 
 load_bioconductor_pkgs <- function(pkg) {
@@ -29,9 +32,12 @@ bioconductor_pkgs <- c(
   "biomaRt", "rhdf5", "tximport", "DESeq2", "apeglm", "RUVSeq", "limma", 
   "edgeR", "csaw", "vsn", "hexbin"
 )
+
 load_bioconductor_pkgs(bioconductor_pkgs)
 
 Sys.setenv("R_REMOTES_NO_ERRORS_FROM_WARNINGS" = TRUE)
 devtools::install_github("cttobin/ggthemr")
+
+# renv::settings$r.version("3.6.1")
 
 renv::snapshot()
